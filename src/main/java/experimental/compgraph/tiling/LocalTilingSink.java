69bdcf0e4949f035e41ee92b96efcab2f24d79b9
MarcelWiedenmann <Marcel.Wiedenmann@uni-konstanz.de>
2016-12-15 12:00:19 +0100

    WIP

--- src/main/java/experimental/compgraph/tiling/LocalTilingSink.java
+++ src/main/java/experimental/compgraph/tiling/LocalTilingSink.java
@@ -1,15 +1,17 @@
 
 package experimental.compgraph.tiling;
 
+import net.imglib2.RandomAccessibleInterval;
+
 import experimental.compgraph.AbstractCompgraphSinkNode;
 import experimental.compgraph.CompgraphEdge;
 import experimental.compgraph.request.TilingRequestable;
 
 public class LocalTilingSink<IO> extends
-	AbstractCompgraphSinkNode<LazyTile<IO>, TilingDataHandle<IO>, TilingRequestable<IO>>
+	AbstractCompgraphSinkNode<RandomAccessibleInterval<IO>, TilingDataHandle<IO>, TilingRequestable<IO>>
 {
 
-	public LocalTilingSink(final CompgraphEdge<LazyTile<IO>> in) {
+	public LocalTilingSink(final CompgraphEdge<RandomAccessibleInterval<IO>> in) {
 		super(in);
 	}
 
@@ -17,73 +19,4 @@ public class LocalTilingSink<IO> extends
 	protected TilingRequestable<IO> getInternal(final TilingDataHandle<IO> inData) {
 		return inData.inner();
 	}
-<<<<<<< HEAD
-=======
-
-	public static class LazyTiling<IO> extends AbstractInterval
-			implements RandomAccessibleInterval<RandomAccessibleInterval<IO>> {
-
-		private LocalTilingSink<IO> sink;
-
-		// TODO: this may extend TiledView
-		public LazyTiling(LocalTilingSink<IO> sink, long[] tileSize, long[] gridDims) {
-			super(gridDims);
-			this.sink = sink;
-		}
-
-		@Override
-		public RandomAccess<RandomAccessibleInterval<IO>> randomAccess() {
-
-			return new LazyTilingAccess<>(sink);
-		}
-
-		public static class LazyTilingAccess<IO> extends Point implements RandomAccess<RandomAccessibleInterval<IO>> {
-
-			private LocalTilingSink<IO> sink;
-
-			public LazyTilingAccess(LocalTilingSink<IO> sink) {
-				this.sink = sink;
-			}
-
-			@Override
-			public RandomAccessibleInterval<IO> get() {
-
-				Tile i = getTile(position);
-
-				TileRequest r = new TileRequest() {
-
-					@Override
-					public Tile key() {
-						return i;
-					}
-				};
-
-				LazyTile<IO> result = sink.get().request(r);
-				return result;
-			}
-
-			public Tile getTile(long[] pos) {
-				return null;
-			}
-
-			@Override
-			public Sampler<RandomAccessibleInterval<IO>> copy() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public RandomAccess<RandomAccessibleInterval<IO>> copyRandomAccess() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		}
-
-		@Override
-		public RandomAccess<RandomAccessibleInterval<IO>> randomAccess(Interval interval) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	}
->>>>>>> 6d6da8a26c1040af8548049fcac6b8fdc54ef87d
 }
