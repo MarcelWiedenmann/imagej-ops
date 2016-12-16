221be032e3bf2f18fa8602449c14c3cc8501adfd
MarcelWiedenmann <Marcel.Wiedenmann@uni-konstanz.de>
2016-12-16 17:41:48 +0100

    WIP

--- src/main/java/experimental/compgraph/tiling/request/TilingBulkRequestable.java
+++ src/main/java/experimental/compgraph/tiling/request/TilingBulkRequestable.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import net.imglib2.Interval;
-import net.imglib2.util.IntervalIndexer;
 
 import experimental.compgraph.tiling.DefaultTile;
 import experimental.compgraph.tiling.LazyTile;
@@ -17,13 +16,55 @@ public class TilingBulkRequestable<I, O> {
 
 	private final ConcurrentHashMap<Long, Tile> queue = new ConcurrentHashMap<>();
 	private final TilingRequestable<I> source;
+	private final long[] gridDims;
+	private final long[] tileDims;
+	// NB: Keep those internal! They must not be modified.
+	private final long[] fullTileMin;
+	private final long[] fullTileMax;
 
-	public TilingBulkRequestable(final TilingRequestable<I> source) {
+	public TilingBulkRequestable(final TilingRequestable<I> source, final long[] gridDims, final long[] tileDims) {
 		this.source = source;
+		this.gridDims = gridDims;
+		this.tileDims = tileDims;
+		this.fullTileMin = new long[tileDims.length];
+		this.fullTileMax = new long[tileDims.length];
+		for (int d = 0; d < fullTileMax.length; d++) {
+			// Implicitly: fullTileMin[d] = 0;
+			fullTileMax[d] = tileDims[d] - 1;
+		}
 	}
 
-	public void request(final Interval i) {
+	public long[] getGridDims() {
+		return gridDims;
+	}
 
+	public long[] getTileDims() {
+		return tileDims;
+	}
+
+	public void request(final Interval interval) {
+		final int n = interval.numDimensions();
+		final long[] min = new long[n];
+		final long[] max = new long[n];
+		interval.min(min);
+		interval.max(max);
+
+		final long[] minIndex = new long[n];
+		final long[] maxIndex = new long[n];
+
+		for (int d = n - 1; d >= 0; --d) {
+			minIndex[d] = normalizedPosition / blockSize[d];
+			maxIndex[d] = offset[d] = normalizedPosition % blockSize[d];
+			flatIndex = flatIndex * source.dimension(d) + tempIndex[d];
+		}
+	}
+
+	public void request(final long flatIndex) {
+		requestInternal(flatIndex, fullTileMin, fullTileMax);
+	}
+
+	public void request(final Tile tile) {
+		requestInternal(tile.flatIndex(), tile.min(), tile.max());
 	}
 
 	private void requestInternal(final long index, final long[] min, final long[] max) {
@@ -40,8 +81,6 @@ public class TilingBulkRequestable<I, O> {
 	}
 
 	private Tile enqueue(final long index, final long[] min, final long[] max) {
-
-		final long i = IntervalIndexer.positionToIndex(position, gridDims);
 		final long[] globalMin = new long[min.length];
 		final long[] globalMax = new long[max.length];
 		for (int d = 0; d < n; d++) {
@@ -86,14 +125,5 @@ public class TilingBulkRequestable<I, O> {
 		}
 		queue.clear();
 		return res;
-	}
-
-	public void request(Tile next) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void request(long idx) {
-		
 	}
 }
